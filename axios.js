3cfd1d6b10e58c3435f5e32258d671a9117e1801
anshulme96@gmail.com <anshulme96@gmail.com>
2023-05-18 07:39:16 +0530

    TODO LIST USNG ASYN AWAIT

--- axios.js
+++ axios.js
@@ -1,14 +1,9 @@
 let input = document.getElementById('Item');
 let description = document.getElementById('description');
 let addTask = document.getElementById('addTask');
-
-let list = document.getElementById('pending');
+let body=document.getElementById('container')
 let completedTaskList=document.getElementById('completed')
 //console.log(completedTaskList)
-
-const checkbtn = document.createElement('button');
-const delBtn = document.createElement('button');
-
 
 window.addEventListener("DOMContentLoaded", getTaskDetails);
 
@@ -24,76 +19,66 @@ function addTaskDetails(e) {
 
     localStorage.setItem(input, JSON.stringify(data));
 
-    axios.post('https://crudcrud.com/api/3747e51f7f6b4b1c9ab47ecf23045c62/todolist', data)
-        .then((response) => {
-            let output = ` <li>
-            ${data.input}   ${data.description}
-            <button id="check" class="checkbtn"><i class="fa fa-check"></i></button>
-            <button id="delete" class="delBtn" ><i class="fa fa-trash"></i></button>
-        </li>`
-            list.innerHTML += output;
-            input.value = "";
-            description.value = "";
-            console.log(response);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    axios.post('https://crudcrud.com/api/a9998faf621847ccbfd9c7986a8be995/todolist', data)
+    .then((response) => {
+        let output = ` <li>
+        ${data.input}   ${data.description}
+        <button id="check" class="checkbtn"><i class="fa fa-check"></i></button>
+        <button id="delete" class="delBtn" ><i class="fa fa-trash"></i></button>
+    </li>`
         
-
+        input.value = "";
+        description.value = "";
+        console.log(response);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+    
+    //post(data)
 }
 
 
 function getTaskDetails() {
-    axios.get('https://crudcrud.com/api/3747e51f7f6b4b1c9ab47ecf23045c62/todolist')
+    axios.get('https://crudcrud.com/api/a9998faf621847ccbfd9c7986a8be995/todolist')
         .then((response) => {
+            const ul = document.createElement('ul');
             response.data.forEach(element => {
-                let output = ` <li id="${element._id}">${element.input}   ${element.description}
-            <button id="check" class="checkbtn"><i class="fa fa-check"></i></button>
-            <button id="delete" class="delBtn" ><i class="fa fa-trash"></i></button>
+                const li=document.createElement('li');
+               
+              let Taskoutput  = ` <li id="${element._id}">${element.input}   ${element.description}
+            <button id="check" class="checkbtn" onclick="updateTaskList()"><i class="fa fa-check"></i></button>
+            <button id="delete" class="delBtn" onclick="deleteTask(${element._id})"><i class="fa fa-trash"></i></button>
             </li>`
-                console.log(output);
-                list.innerHTML += output;
-
+                li.innerHTML=Taskoutput
+                ul.appendChild(li)
+                console.log(Taskoutput);
+               
+                
             });
+             document.body.appendChild(ul);
         })
 }
 
-
-// function checkbtn(e){
-//     if(e.target.ClassList.contains('checkbtn')){
-//         const item=e.target.parentElement;
-//         const id=item.getAttribute('id')
-//     }
-// }
-
-list.addEventListener('click', deleteTask)
-function deleteTask(e) {
-    if (e.target.classList.contains("delBtn")) {
-        let item = e.target.parentElement;
-        const id = item.getAttribute('id');
-      //  console.log(id);
-      //item.remove();
-        axios.delete(`https://crudcrud.com/api/3747e51f7f6b4b1c9ab47ecf23045c62/todolist/${id}`)
+function deleteTask(id) {
+    console.log(1)
+        axios.delete(`https://crudcrud.com/api/a9998faf621847ccbfd9c7986a8be995/todolist/${id}`)
             .then((response) => {
-                item.remove();
+               console.log(response)
             }).catch(err => console.log(err));
     }
-}
-
-list.addEventListener('click',updateTaskList);
 
   function updateTaskList(e){
-   
+    console.log(2);
     if(e.target.classList.contains("checkbtn")){
         var item=e.target.parentElement;
         var id=item.getAttribute('id')
-        axios.delete(`https://crudcrud.com/api/3747e51f7f6b4b1c9ab47ecf23045c62/todolist/${id}`)
+        console.log(1);
+        axios.delete(`https://crudcrud.com/api/a9998faf621847ccbfd9c7986a8be995/todolist/${id}`)
         .then((response) => {
-           item.remove();
-           console.log(item);
-            completedTaskList.appendChild(item);
-            checkbtn.style.display='none';
+         console.log(item);
+         item.remove();
+        completedTaskList.appendChild(item);
            
         }).catch(err => console.log(err));
     }
